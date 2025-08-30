5ea062adf2591899092c06cbe86532163d35350a
Shawn Dao <shawn.dao@gemcommerce.com>
2025-08-30 11:13:16 +0700

    Update CORS configuration to allow specific origins for enhanced security and local development support.

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ const app = express();
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use(cors({ origin: true, credentials: true }));
+app.use(
+  cors({
+    origin: ['https://thcsdongthanhoanlong.edu.vn', 'http://localhost:3000'],
+    credentials: true,
+  })
+);
 
 if (!fs.existsSync(ENV.UPLOAD_DIR))
   fs.mkdirSync(ENV.UPLOAD_DIR, { recursive: true });
